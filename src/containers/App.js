b345efc844b7a8086c201e2995b2aedc1b123d1f
Sento <Sornosa>
2020-08-09 01:19:08 +0200

    created the project

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList.js';
+import { robots } from '../robots.js';
+import SearchBox from '../components/SearchBox.js';
+import './App.css';
+import Scroll from '../components/Scroll.js';
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => {this.setState({ robots: users })});
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+
+	render() {
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+		return (!robots.length) ? 
+		<h1> Loading... </h1>
+		:
+		(
+		<div className='tc'>
+			<h1 className='f1'> Robo Friends </h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
+		</div>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode> <App />
+  	</React.StrictMode>, document.getElementById('root')
 );
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
